db1318f2f7eff4c53f5390b1908ba1da1f7d82cc
Stepan Skriabin <stepan.skrjabin@gmail.com>
2022-04-14 23:33:23 +0300

    feat: add logging and catching Exception in publisher

--- src/client.py
+++ src/client.py
@@ -3,6 +3,12 @@ Test client
 """
 
 import paho.mqtt.client as mqtt
+import logging
+
+logging.basicConfig(filename="info.log",
+                    filemode='a',
+                    level=logging.NOTSET)
+logger = logging.getLogger(__name__)
 
 SERVER = "50b39c42c0ce4e079d9694e03cf5b2c6.s1.eu.hivemq.cloud"
 PORT = 8883
@@ -10,35 +16,38 @@ PORT = 8883
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(_client, userdata, flags: dict, rc: str, properties=None):
-    print(f"Connected with result code: {rc}, flags: {flags['session present']}")
+    print(f"Connect established with code: {rc}, flags: {flags['session present']}")
 
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
-    _client.subscribe("/test-home/#")
+    _client.subscribe("/test-home/#", 0)
 
 
 def on_disconnect(_client, userdata, rc, properties=None):
-    print(f"USERDATA: {userdata}, RC: {rc}")
+    print(f"Connect broken with code: {rc}, userdata {userdata}")
 
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(_client, userdata, msg):
-    print(f"MESSAGE TOPIC={msg.topic}",
-          f"- PAYLOAD={str(msg.payload)}",
-          f"- QOS={msg.qos}",
-          f"- RETAIN={msg.retain}")
+    print(f"MESSAGE RECIEVED: TOPIC={msg.topic}, ",
+          f"PAYLOAD={str(msg.payload)}, ",
+          f"QOS={msg.qos}, ",
+          f"RETAIN={msg.retain}")
+    print("===================================")
 
 
-client = mqtt.Client(client_id="666",
+client = mqtt.Client(client_id="67567567567567",
                      userdata=None,
-                     protocol=mqtt.MQTTv5,
+                     protocol=mqtt.MQTTv311,
                      transport="tcp")
+client.enable_logger(logger)
 client.tls_set(tls_version=mqtt.ssl.PROTOCOL_TLS)
 client.username_pw_set("stepan", "1q2w3E4R")
 
 client.connect(host=SERVER,
                port=PORT,
-               keepalive=60)
+               keepalive=60,
+               clean_start=mqtt.MQTT_CLEAN_START_FIRST_ONLY)
 client.on_connect = on_connect
 client.on_message = on_message
 client.on_disconnect = on_disconnect
--- src/pub.py
+++ src/pub.py
@@ -4,33 +4,43 @@ Test publisher
 
 import paho.mqtt.publish as pub
 import paho.mqtt.client as mqtt
+import json
 
 SERVER = "50b39c42c0ce4e079d9694e03cf5b2c6.s1.eu.hivemq.cloud"
 PORT = 8883
 
+PTEST = {"uuid": "888",
+         "type": "test",
+         "bool": True,
+         "null": None}
+
 AUTH = {"username": "stepan", "password": "1q2w3E4R"}
 TLS = {"tls_version": mqtt.ssl.PROTOCOL_TLS}
 
-pub.single("/test-home/message",
-           payload="test4",
-           qos=0,
-           retain=True,
-           hostname=SERVER,
-           port=PORT,
-           client_id="666",
-           auth=AUTH,
-           tls=TLS,
-           protocol=mqtt.MQTTv5,
-           transport="tcp")
-
-pub.single("/test-home/callback",
-           payload="callback test",
-           qos=0,
-           retain=True,
-           hostname=SERVER,
-           port=PORT,
-           client_id="666",
-           auth=AUTH,
-           tls=TLS,
-           protocol=mqtt.MQTTv5,
-           transport="tcp")
+try:
+    pub.single("/test-home/message",
+               payload=json.dumps(PTEST),
+               qos=2,
+               retain=True,
+               hostname=SERVER,
+               port=PORT,
+               client_id="",
+               auth=AUTH,
+               tls=TLS,
+               protocol=mqtt.MQTTv311,
+               transport="tcp")
+    pub.single("/test-home/callback",
+               payload="TEST18",
+               qos=2,
+               retain=False,
+               hostname=SERVER,
+               port=PORT,
+               client_id="",
+               auth=AUTH,
+               tls=TLS,
+               protocol=mqtt.MQTTv311,
+               transport="tcp")
+except Exception as err:
+    print(f"Message not sent, because {err}")
+else:
+    print("all message sent")
