1c70de44c1d847aa2096941a835cbe06eec17d9b
Stepan Skriabin <stepan.skrjabin@gmail.com>
2022-05-03 22:30:01 +0300

    fix: delete LOGGER

--- src/main_settings.py
+++ src/main_settings.py
@@ -3,17 +3,10 @@ import uuid
 __version__ = 'v0.1'
 __client_id__ = '123456'
 
-LOGGER = {'notset': 0,
-          'debug': 10,
-          'info': 20,
-          'warning': 30,
-          'error': 40,
-          'critical': 50}
-
 # Config MQTT broker
 SERVER = "50b39c42c0ce4e079d9694e03cf5b2c6.s1.eu.hivemq.cloud"
-AUTH = {"username": "stepan",
-        "password": "1q2w3E4R"}
+AUTH = {"username": "",
+        "password": ""}
 
 # Config devices
 ID = uuid.uuid1()
