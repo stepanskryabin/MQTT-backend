8d8f9086d46ab23596e604f8b33db123e69b6e3e
Stepan Skriabin <stepan.skrjabin@gmail.com>
2022-05-06 10:47:01 +0300

    fixed: fix config devices, add multiple publisher

--- src/main_settings.py
+++ src/main_settings.py
@@ -1,17 +1,19 @@
-import uuid
 
-__version__ = 'v0.1'
+
+__version__ = 'v0.1.2'
 __client_id__ = '123456'
 
 # Config MQTT broker
 SERVER = "50b39c42c0ce4e079d9694e03cf5b2c6.s1.eu.hivemq.cloud"
-AUTH = {"username": "",
-        "password": ""}
 
 # Config devices
-ID = uuid.uuid1()
-TYPE = 'channel'
-NAME = 'Канал'
+TYPE = 'panel'
+NAME1 = 'СП1'
+NAME2 = 'СП1'
+#
+LOCKED = True
+PAGE = "1111-22222-3333"
+
 
 LOG_FORMAT = "%(asctime)s:%(levelname)s-(%(module)s:%(funcName)s): %(message)s"
 DATE_FORMAT = '%Y-%m-%d %I:%M:%S:%p'
--- src/pub.py
+++ src/pub.py
@@ -2,17 +2,20 @@
 Test publisher
 """
 
+
 import random
-import time
 import json
+import uuid
 import argparse
 import logging
 
 import paho.mqtt.publish as pub
 import paho.mqtt.client as mqtt
 
-from main_settings import ID, TYPE, NAME, SERVER, AUTH, __version__, \
-    LOG_FORMAT, DATE_FORMAT
+from main_settings import SERVER, __version__, LOG_FORMAT, DATE_FORMAT
+
+ID1 = uuid.uuid4()
+ID2 = uuid.uuid4()
 
 
 def description(publisher_type: str = 'button',
@@ -21,16 +24,26 @@ def description(publisher_type: str = 'button',
             'name': publisher_name}
 
 
-def state_control(value=0.0):
-    return {"value": value}
+def state(locked=False,
+          page=None):
+    return {"locked": locked,
+            "page": page}
 
 
-def config(raise_time=0.0,
-           fall_time=0.0,
-           sacn_universe=random.randrange(1, 63999, 1),
-           sacn_priority=100,
-           sacn_name="APOLLO",
-           sacn_pps=0):
+def control(lock=False,
+            screen_off=None,
+            set_page=''):
+    return {"lock": lock,
+            "screen_off": screen_off,
+            "set_page": set_page}
+
+
+def page(raise_time=0.0,
+         fall_time=0.0,
+         sacn_universe=random.randrange(1, 63999, 1),
+         sacn_priority=100,
+         sacn_name="APOLLO",
+         sacn_pps=0):
     return {"raise_time": raise_time,
             "fall_time": fall_time,
             "sacn_universe": sacn_universe,
@@ -39,66 +52,42 @@ def config(raise_time=0.0,
             "sacn_pps": sacn_pps}
 
 
-def main(connection: bool,
-         server: str,
-         auth: dict):
+def main(server: str,
+         username: str,
+         password: str) -> None:
 
-    if connection:
-        PORT = 8884
-        SOCKET = "websockets"
-    else:
-        PORT = 8883
-        SOCKET = "tcp"
-
-    try:
-        pub.single(f"/{ID}/desc",
-                   payload=json.dumps(description(TYPE, NAME)),
-                   qos=2,
-                   retain=False,
-                   hostname=server,
-                   port=PORT,
-                   client_id="",
-                   auth=auth,
-                   tls={"tls_version": mqtt.ssl.PROTOCOL_TLS},
-                   protocol=mqtt.MQTTv5,
-                   transport=SOCKET)
-        pub.single(f"/{ID}/state",
-                   payload=json.dumps(state_control()),
-                   qos=2,
-                   retain=False,
-                   hostname=server,
-                   port=PORT,
-                   client_id="",
-                   auth=auth,
-                   tls={"tls_version": mqtt.ssl.PROTOCOL_TLS},
-                   protocol=mqtt.MQTTv5,
-                   transport=SOCKET)
-        pub.single(f"/{ID}/control",
-                   payload=json.dumps(state_control()),
-                   qos=2,
-                   retain=False,
-                   hostname=server,
-                   port=PORT,
-                   client_id="",
-                   auth=auth,
-                   tls={"tls_version": mqtt.ssl.PROTOCOL_TLS},
-                   protocol=mqtt.MQTTv5,
-                   transport=SOCKET)
-        pub.single(f"/{ID}/config",
-                   payload=json.dumps(config()),
-                   qos=2,
-                   retain=False,
-                   hostname=server,
-                   port=PORT,
-                   client_id="",
-                   auth=auth,
-                   tls={"tls_version": mqtt.ssl.PROTOCOL_TLS},
-                   protocol=mqtt.MQTTv5,
-                   transport=SOCKET)
-    except Exception as err:
-        logger.error(f"Message not sent, because {err}")
-    else:
-        logger.info(f"All message sent, across {SOCKET}")
+         qos = 0
+         retain = False
+         _message = [{"topic": f"/{ID1}/desc",
+                     "payload": json.dumps(description('panel', 'СП1')),
+                     "qos": qos,
+                     "retain": retain},
+#                     {"topic": f"/{ID1}/state",
+#                     "payload": json.dumps(state(True, "111-222-333")),
+#                     "qos": qos,
+#                     "retain": retain},
+                     {"topic": f"/{ID2}/desc",
+                     "payload": json.dumps(description('panel', 'СП2')),
+                     "qos": qos,
+                     "retain": retain}#,
+#                     {"topic": f"/{ID2}/state",
+#                     "payload": json.dumps(state(False, "222-333-444")),
+#                     "qos": qos,
+#                     "retain": retain}
+                        ]
+         try:
+            pub.multiple(msgs=_message,
+                         hostname=server,
+                         port=8883,
+                         auth={'username': username,
+                               'password': password},
+                         tls={"tls_version": mqtt.ssl.PROTOCOL_TLS},
+                         protocol=mqtt.MQTTv5,
+                         transport="tcp")
+         except Exception as err:
+            logger.error(f"Message not sent, because {err}")
+         else:
+            logger.info("All message sent, across tcp")
 
 
 if __name__ == "__main__":
@@ -107,13 +96,17 @@ if __name__ == "__main__":
                                      description="Publisher for testing"
                                                  "MQTT connection",
                                      epilog="<stepan.skrjabin@gmail.com>")
-    parser.add_argument("--websocket",
-                        action='store_true',
-                        default=False,
-                        help="establish a prioritized protocol is websocket")
+    parser.add_argument("--user",
+                        action='store',
+                        help="имя для авторизация на брокере")
+    parser.add_argument("--password",
+                        action='store',
+                        help="пароль для авторизация на брокере")
     parser.add_argument("--log",
                         nargs=1,
+                        type=int,
                         choices=range(0, 60, 10),
+                        default=0,
                         help="config level logging. \n"
                         "Where 0 - notset, 10 - debug, 20 - info,"
                         "30 - warning, 40 - error, 50 - critical")
@@ -123,17 +116,18 @@ if __name__ == "__main__":
     arguments = parser.parse_args()
 
     logging.basicConfig(filename="pub.log",
-                        filemode='a',
+                        filemode='w',
                         format=LOG_FORMAT,
                         datefmt=DATE_FORMAT,
-                        level=arguments.log[0])
+                        level=arguments.log)
     logger = logging.getLogger(__name__)
 
     logger.info("Run publisher")
-    logger.info(f"Run with websockets={arguments.websocket}")
 
-    while True:
-        main(arguments.websocket,
-             SERVER,
-             AUTH)
-        time.sleep(10)
+
+    a = 0
+    while a < 1:
+        main(server=SERVER,
+             username=arguments.user,
+             password=arguments.password)
+        a = 1
